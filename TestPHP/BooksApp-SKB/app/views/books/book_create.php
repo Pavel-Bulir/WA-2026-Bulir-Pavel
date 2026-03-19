c00eb8c78a942ddbd101f28af50cc0c9c389bfa2
Pavel-Bulíř <pavel.bulir@tul.cz>
2026-03-19 15:29:38 +0100

    #5 php - vytvoření dalších polí

--- TestPHP/BooksApp-SKB/app/views/books/book_create.php
+++ TestPHP/BooksApp-SKB/app/views/books/book_create.php
@@ -18,12 +18,16 @@
             <form action="">
                 <div>
                     <div>
-                        <label for="title">Název knihy<span>*</span></label>
-                        <input type="text" id="title" name="title" required>
+                        <label for="title">Název knihy<span> *</span></label>
+                        <input type="text" id="title" name="title" required> <!-- required = nejde odeslat, dokud nebude vyplněný -->
                     </div>
                     <div>
-                        <label for="author">Autor knihy<span>*</span></label>
-                        <input type="text" id="author" name="author" required>
+                        <label for="author">Autor knihy<span> *</span></label>
+                        <input type="text" id="author" name="author" placeholder="Příjmení, jméno" required> <!-- id je pro javascript, name je pro css| Placeholder dělá šedej text v poli na psaní, který zmizí, když se do něj začne psát (jako příklad co se do toho píše) -->
+                    </div>
+                     <div>
+                        <label for="isbn">ISBN</label>
+                        <input type="number" id="isbn" name="isbn" > 
                     </div>
                     <div>
                         <label for="category">Kategorie</label>
@@ -34,8 +38,28 @@
                         <input type="text" id="subcategory" name="subcategory">
                     </div>
                     <div>
-                        <label for="year">Rok vydání<span>*</span></label>
+                        <label for="year">Rok vydání<span> *</span></label>
                         <input type="number" id="year" name="year" required>
+                    </div>
+                    <div>
+                        <label for="price">Cena</label>
+                        <input type="number" id="price" name="price" step ="0.5"> <!-- step = když měníme čísla v poli pomocí šipek, tak step nám ovlivňuje o kolik se to změní zmáčknutím šipky -->
+                    </div>
+                     <div>
+                        <label for="link"><span>Odkaz</span></label>
+                        <input type="number" id="link" name="link" >
+                    </div>
+                     <div>
+                        <label for="description">Popis knihy</label>
+                        <textarea name="description" id="description" rows="10">Napište popis knihy</textarea> <!-- textarea, vytvoří velké pole, do kterého se může psát, je to stejné jako input jenom větší | rows a cols, nám říká jak ze začátku bude velké to pole -->
+                    </div>
+                    <div>
+                        <label >Obrázky (můžete nahrát více)</label>
+                        <label >
+                            <span >Klikni pro výběr souborů</span>
+                            <span >JPG / PNG / WebP – více souborů najednou</span>
+                            <input type="file" id="images" name="images[]" multiple accept="image/*" class="hidden"> <!-- images[] = bude tam nahráno více obrázků | type je file = není to ani text ani číslo ale bude se tam vkládat nějakej soubor -->
+                        </label>
                     </div>
                 </div>
 
