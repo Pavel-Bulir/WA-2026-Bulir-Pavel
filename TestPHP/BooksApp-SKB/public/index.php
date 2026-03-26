a9226668d40d5c7662da82cd90fe4c921dbf6c07
Pavel-Bulíř <pavel.bulir@tul.cz>
2026-03-26 14:51:31 +0100

    #8 zkpírovaný kód

--- TestPHP/BooksApp-SKB/public/index.php
+++ TestPHP/BooksApp-SKB/public/index.php
@@ -1,8 +1,12 @@
 <?php
 
+//pro účely cýuky a ladění na lokálmním serveru (např. XAMPP)
+// je vhodné zapnout kompletní zovrazování chyb.
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+//Načtení třiídy routeru, která
 require_once '../core/App.php';
 $app = new App();
